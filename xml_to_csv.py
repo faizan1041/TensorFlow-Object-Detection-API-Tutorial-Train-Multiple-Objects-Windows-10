e8e4e419cfd2623e9edf0348ec346fd4bbe31987
faizan <faizan1041@gmail.com>
2018-10-04 17:46:22 +0500

    new xml to csv ..

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -26,10 +26,10 @@ def xml_to_csv(path):
 
 
 def main():
-    for folder in ['train','test']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+    for directory in ['train','test']:
+        image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
         print('Successfully converted xml to csv.')
 
 
